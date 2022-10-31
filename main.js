a6d15994a171a217cd3f4a7067b2f4d260b54372
Lucas Mendes <lucsmends09@gmail.com>
2022-10-31 14:53:22 -0300

    inicio responsividade

---
+++ main.js
@@ -0,0 +1,107 @@
+const buttonOrcamento = document.querySelector('.buttonOrcamento')
+const cardCFTV = document.getElementById('cardCFTV')
+const cardFibra = document.getElementById('cardFibra')
+const cardOrganizacao = document.getElementById('cardOrganizacao')
+const cardCabeamento = document.getElementById('cardCabeamento')
+
+const btnFibra = document.getElementById('iconFibra')
+const btnCFTV = document.getElementById('iconCFTV')
+const btnOrganizacao = document.getElementById('iconOrganizacao')
+const btnCabeamento = document.getElementById('iconCabeamento')
+
+
+btnFibra.addEventListener('click', mudarCardFibra )
+btnCFTV.addEventListener('click', mudarCardCFTV )
+btnOrganizacao.addEventListener('click', mudarCardOrganizacao )
+btnCabeamento.addEventListener('click', mudarCardCabeamento )
+
+function mudarCardFibra(){
+    const actualStyleCardCFTV = getComputedStyle(cardCFTV).display
+    const actualStyleCardOrganizacao = getComputedStyle(cardOrganizacao).display
+    const actualStyleCardCabeamento = getComputedStyle(cardCabeamento).display
+
+    if(actualStyleCardCFTV === 'block'){
+        cardCFTV.style.display = 'none'
+        btnCFTV.classList.remove('clicado')
+        cardFibra.style.display = 'block'
+        btnFibra.classList.add('clicado')
+    } else if(actualStyleCardOrganizacao ==='block'){
+        cardOrganizacao.style.display = 'none'
+        btnOrganizacao.classList.remove('clicado')
+        cardFibra.style.display = 'block'
+        btnFibra.classList.add('clicado')
+    } else if(actualStyleCardCabeamento === 'block'){
+        cardCabeamento.style.display = 'none'
+        btnCabeamento.classList.remove('clicado')
+        cardFibra.style.display = 'block'
+        btnFibra.classList.add('clicado')
+    }
+}
+
+function mudarCardCFTV(){
+    const actualStyleCardFibra = getComputedStyle(cardFibra).display
+    const actualStyleCardOrganizacao = getComputedStyle(cardOrganizacao).display
+    const actualStyleCardCabeamento = getComputedStyle(cardCabeamento).display
+
+    if(actualStyleCardFibra === 'block'){
+        cardFibra.style.display = 'none'
+        btnFibra.classList.remove('clicado')
+        cardCFTV.style.display = 'block'
+        btnCFTV.classList.add('clicado')
+    }  else if(actualStyleCardOrganizacao ==='block'){
+        cardOrganizacao.style.display = 'none'
+        btnOrganizacao.classList.remove('clicado')
+        cardCFTV.style.display = 'block'
+        btnCFTV.classList.add('clicado')
+    } else if(actualStyleCardCabeamento === 'block'){
+        cardCabeamento.style.display = 'none'
+        btnCabeamento.classList.remove('clicado')
+        cardCFTV.style.display = 'block'
+        btnCFTV.classList.add('clicado')
+    }
+}
+
+function mudarCardOrganizacao(){
+    const actualStyleCardFibra = getComputedStyle(cardFibra).display
+    const actualStyleCardCFTV = getComputedStyle(cardCFTV).display
+    const actualStyleCardCabeamento = getComputedStyle(cardCabeamento).display
+
+    if(actualStyleCardFibra === 'block'){
+        cardFibra.style.display = 'none'
+        btnFibra.classList.remove('clicado')
+        cardOrganizacao.style.display = 'block'
+        btnOrganizacao.classList.add('clicado')
+    }  else if(actualStyleCardCFTV ==='block'){
+        cardCFTV.style.display = 'none'
+        btnCFTV.classList.remove('clicado')
+        cardOrganizacao.style.display = 'block'
+        btnOrganizacao.classList.add('clicado')
+    } else if(actualStyleCardCabeamento === 'block'){
+        cardCabeamento.style.display = 'none'
+        btnCabeamento.classList.remove('clicado')
+        cardOrganizacao.style.display = 'block'
+        btnOrganizacao.classList.add('clicado')
+    }
+}
+function mudarCardCabeamento(){
+    const actualStyleCardFibra = getComputedStyle(cardFibra).display
+    const actualStyleCardOrganizacao = getComputedStyle(cardOrganizacao).display
+    const actualStyleCardCFTV = getComputedStyle(cardCFTV).display
+
+    if(actualStyleCardFibra === 'block'){
+        cardFibra.style.display = 'none'
+        btnFibra.classList.remove('clicado')
+        cardCabeamento.style.display = 'block'
+        btnCabeamento.classList.add('clicado')
+    }  else if(actualStyleCardOrganizacao ==='block'){
+        cardOrganizacao.style.display = 'none'
+        btnOrganizacao.classList.remove('clicado')
+        cardCabeamento.style.display = 'block'
+        btnCabeamento.classList.add('clicado')
+    } else if(actualStyleCardCFTV === 'block'){
+        cardCFTV.style.display = 'none'
+        btnCFTV.classList.remove('clicado')
+        cardCabeamento.style.display = 'block'
+        btnCabeamento.classList.add('clicado')
+    }
+}
